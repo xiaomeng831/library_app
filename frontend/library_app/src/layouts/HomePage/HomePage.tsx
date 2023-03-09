2d2b3abb6c13407e06bea5669cd9c447603de79f
xiaomeng831 <78976407+xiaomeng831@users.noreply.github.com>
2023-03-09 14:28:34 -0500

    add router to homepage and searchpage

--- frontend/library_app/src/layouts/HomePage/HomePage.tsx
+++ frontend/library_app/src/layouts/HomePage/HomePage.tsx
@@ -1,9 +1,13 @@
+import { useEffect } from "react";
 import { Carousel } from "./components/Carousel";
 import { ExploreToBooks } from "./components/ExploreToBooks";
 import { Heros } from "./components/Heros";
 import { LibraryServices } from "./components/LibraryServices";
 
 export const HomePage = () => {
+    useEffect(()=>{
+        window.scrollTo(0, 0);
+    },[])
     return (
         <>
             <ExploreToBooks />
--- frontend/library_app/src/layouts/HomePage/components/ExploreToBooks.tsx
+++ frontend/library_app/src/layouts/HomePage/components/ExploreToBooks.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export const ExploreToBooks = () => {
     return (
         <div className="p-5 mb-4 bg-dark header">
@@ -6,9 +8,9 @@ export const ExploreToBooks = () => {
                 <div>
                     <h1 className="display-5 fw-bold">Happy Reading!</h1>
                     <p className="col-md-8 fs-4">The more that you read, the more things you will know!</p>
-                    <a type='button' className="btn main-color btn-lg text-white" href='#'>
+                    <Link type='button' className="btn main-color btn-lg text-white" to='/search'>
                         Explore top Books
-                    </a>
+                    </Link>
                 </div>
             </div>
         </div>
