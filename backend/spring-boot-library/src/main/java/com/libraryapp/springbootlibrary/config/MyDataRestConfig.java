9a7391b4e46d4ddcb9952f1215dcf7bd6e0f12fc
xiaomeng831 <78976407+xiaomeng831@users.noreply.github.com>
2023-03-15 14:55:48 -0400

    completed the review page and rewive backend api

--- backend/spring-boot-library/src/main/java/com/libraryapp/springbootlibrary/config/MyDataRestConfig.java
+++ backend/spring-boot-library/src/main/java/com/libraryapp/springbootlibrary/config/MyDataRestConfig.java
@@ -1,6 +1,7 @@
 package com.libraryapp.springbootlibrary.config;
 
 import com.libraryapp.springbootlibrary.entity.Book;
+import com.libraryapp.springbootlibrary.entity.Review;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -18,8 +19,10 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
         HttpMethod[] theUnsupportedActions = {HttpMethod.POST, HttpMethod.PUT, HttpMethod.PATCH, HttpMethod.DELETE};
 
         config.exposeIdsFor(Book.class);
+        config.exposeIdsFor(Review.class);
 
         disableHttpMethods(Book.class, config, theUnsupportedActions);
+        disableHttpMethods(Review.class, config, theUnsupportedActions);
 
         /* Configure CORS Mapping*/
         cors.addMapping(config.getBasePath() + "/**").allowedOrigins(theAllowedOrigins);
--- backend/spring-boot-library/src/main/java/com/libraryapp/springbootlibrary/dao/ReviewRepository.java
+++ backend/spring-boot-library/src/main/java/com/libraryapp/springbootlibrary/dao/ReviewRepository.java
@@ -1,2 +1,13 @@
-package com.libraryapp.springbootlibrary.dao;public interface ReviewRepository {
+package com.libraryapp.springbootlibrary.dao;
+
+import com.libraryapp.springbootlibrary.entity.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    Page<Review> findByBookId(@Param("book_id") Long bookId,
+                              Pageable pageable);
 }
