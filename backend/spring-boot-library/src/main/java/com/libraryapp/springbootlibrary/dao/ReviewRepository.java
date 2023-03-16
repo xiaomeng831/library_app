4ec47ffbd2fa68507e35c8a13ab44e9b70ff4385
xiaomeng831 <78976407+xiaomeng831@users.noreply.github.com>
2023-03-16 16:02:23 -0400

    completed Reviews API

--- backend/spring-boot-library/src/main/java/com/libraryapp/springbootlibrary/dao/ReviewRepository.java
+++ backend/spring-boot-library/src/main/java/com/libraryapp/springbootlibrary/dao/ReviewRepository.java
@@ -10,4 +10,6 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
 
     Page<Review> findByBookId(@Param("book_id") Long bookId,
                               Pageable pageable);
+
+    Review findByUserEmailAndBookId(String userEmail, Long bookId);
 }
